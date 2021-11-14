6425b1c3c2aeafe1aef8630d134c9fab09a92e7a
tshak <github@tshak.net>
2021-11-14 15:19:47 +0100

    test

--- gcp-bucket/index.ts
+++ gcp-bucket/index.ts
@@ -1,13 +1,11 @@
 import * as storage from "@pulumi/google-native/storage/v1";
 import * as pulumi from "@pulumi/pulumi";
 
-const config = new pulumi.Config("google-native");
-const project = config.require("project");
+const config = new pulumi.Config();
 const bucketName = `platpoc-${config.require("bucketName")}`;
 
 const bucket = new storage.Bucket("bucket", {
-  name: bucketName,
-  project: project,
+  name: bucketName
 });
 
 export const bucketLink = bucket.selfLink;
